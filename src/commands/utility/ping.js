8b0def5d101e8a03f712ebcba2ed1c31022ee7bf
Nhu Phuc <phucho0237@gmail.com>
2023-01-27 10:08:18 +0000

    ping command update

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,14 +1,36 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const {
+   SlashCommandBuilder,
+   PermissionFlagsBits,
+   ChatInputCommandInteraction,
+   Client,
+   EmbedBuilder,
+} = require("discord.js");
 
 module.exports = {
    data: new SlashCommandBuilder()
       .setName("ping")
-      .setDescription("Response with pong")
+      .setDescription("Replies with the bot ping")
       .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+   /**
+    *
+    * @param {ChatInputCommandInteraction} interaction
+    * @param {Client} client
+    */
    async execute(interaction, client) {
-      interaction.reply({
-         content: "Pong",
-         ephemeral: true,
+      await interaction.deferReply();
+
+      const reply = await interaction.fetchReply();
+      const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+      interaction.editReply({
+         embeds: [
+            new EmbedBuilder()
+               .setColor("Aqua")
+               .setTitle("🏓 Pong!")
+               .setDescription(
+                  `Client: \`${ping}ms\`\nWebsocket: \`${client.ws.ping}ms\``
+               ),
+         ],
       });
    },
 };
