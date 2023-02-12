72c2c7febdf59f6b28837e16ffb616ec492c88e6
Nhu Phuc <phucho0237@gmail.com>
2023-02-12 01:53:57 +0000

    fixed logger

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,7 @@
 const { Client, ActivityType } = require("discord.js");
 const mongoose = require("mongoose");
 
-const logger = require("../../functions/logger");
+const logger = require("../../utils/logger");
 
 module.exports = {
    name: "ready",
@@ -21,19 +21,20 @@ module.exports = {
          status: "dnd",
       });
 
-      logger.client(`Logged in as ${bot.user.tag}`);
+      logger.info(`[CLIENT] | Logged in as ${bot.user.tag}`);
 
       if (bot.config.db.url) {
          mongoose.set("strictQuery", false);
          mongoose
             .connect(bot.config.db.url)
-            .then(() => logger.db("Connected to MongoDB server"))
+            .then(() => logger.info("[DB] | Connected to MongoDB server"))
             .catch(err => {
                logger.error(
-                  "Error when trying to connect to MongoDB server\n",
+                  "[DB] | Error when trying to connect to MongoDB server\n",
                   err
                );
             });
+      } else {
       }
    },
 };
